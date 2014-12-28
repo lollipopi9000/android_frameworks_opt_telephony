a06a0a6213fefeef352f724b57a567c6223a13db
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-28 18:53:11 +0700

    UiccController: add back registerForOn
    It was removed in a77b4218dc29c2cda3eea69e6642821598849898, but this is
    required for many devices.
    
    Change-Id: I5fabd6dd75505aa3f6433437312392805485c4dc

--- src/java/com/android/internal/telephony/uicc/UiccController.java
+++ src/java/com/android/internal/telephony/uicc/UiccController.java
@@ -135,6 +135,7 @@ public class UiccController extends Handler {
                 mCis[i].registerForOn(this, EVENT_ICC_STATUS_CHANGED, index);
             }
             mCis[i].registerForNotAvailable(this, EVENT_RADIO_UNAVAILABLE, index);
+            mCis[i].registerForOn(this, EVENT_ICC_STATUS_CHANGED, index);
             mCis[i].registerForIccRefresh(this, EVENT_SIM_REFRESH, index);
         }
     }
